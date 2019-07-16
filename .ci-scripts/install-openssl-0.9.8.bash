540b19caa9f226b0804f84b643ceb51c8354a0bb
Sean T. Allen <sean@monkeysnatchbanana.com>
2019-07-15 21:11:31 -0400

    Make it work?

--- .ci-scripts/install-openssl-0.9.8.bash
+++ .ci-scripts/install-openssl-0.9.8.bash
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-wget https://www.openssl.org/source/openssl-0.9.8.tar.gz \
-   && tar xf openssl-0.9.8.tar.gz \
-   && cd openssl-0.9.8 \
-   && ./config \
-   && make \
-   && make install \
-   && cd .. \
-   && rm -rf openssl-0.9.8*
+apt-get install -y libssl-dev
